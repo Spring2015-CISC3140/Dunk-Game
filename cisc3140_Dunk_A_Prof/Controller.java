8c71501fa17ebe7db6370308b8a6dfffc382a275
RARJEF <larinfirelight@gmail.com>
2015-03-07 11:05:00 -0500

    Added listener interfaces to Controller class.

--- cisc3140_Dunk_A_Prof/Controller.java
+++ cisc3140_Dunk_A_Prof/Controller.java
@@ -1,11 +1,20 @@
 package cisc3140_Dunk_A_Prof;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+
+import javax.swing.event.MouseInputListener;
+
 /**
 * 
 * Controller code
+* Listens for things, then sends them over to the model.
+* This stuff can also be accomplished via buttons, but atm am unsure of how to implement this in MVC
 * 
 **/
 
-public class Controller {
+public class Controller implements MouseInputListener, KeyListener {
 	
 	Model model;
 	
@@ -15,5 +24,55 @@ public class Controller {
 	
 	public Controller(Model model){
 		this.model = model;
+	}
+
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
