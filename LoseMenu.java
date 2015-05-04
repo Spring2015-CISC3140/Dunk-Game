97ea6b302e96a11ee3c82ee93f006afc8a21e4b6
AlexandraYepifanova <alexyepifanova269399@gmail.com>
2015-05-04 19:56:31 -0400

    Update LoseMenu.java
    added images to buttons

--- LoseMenu.java
+++ LoseMenu.java
@@ -12,7 +12,8 @@ public class LoseMenu extends Pane {
 
     private final double HEIGHT = 600;
     private final double WIDTH = 800;
-    boolean closeGame = true;
+    boolean closeGame = false;
+    boolean restartGame=false;
     private AnchorPane apane = new AnchorPane();
 
     LoseMenu() {
@@ -23,19 +24,48 @@ public class LoseMenu extends Pane {
         LoserBanner();
         super.getChildren().add(apane);
         
+        Image restartImage;//try to get restart image from class path, if not found then load it from url
+        try{
+            restartImage=new Image(getClass().getResource("Media/restart.png").toExternalForm());
+        }
+        catch(Exception e){restartImage=new Image("https://openclipart.org/image/800px/svg_to_png/212123/rodentia-icons_view-refresh.png");}
+        
+        ImageView restartView=new ImageView(restartImage);
+        restartView.setFitHeight(20);
+        restartView.setFitWidth(20);        
+        
         //Adds Restart button and quit game buttons.
-        Button RestartGame = new Button("Restart?");
+        Button RestartGame = new Button("Restart?",restartView);
         RestartGame.setPrefSize(120, 50);
         RestartGame.setStyle("-fx-background-color: #3ADF00;");
         RestartGame.setTextFill(Color.BLUE);
         
+        RestartGame.setOnMouseClicked(e ->{
+            restartGame=true;
+        });        
+        
         AnchorPane.setBottomAnchor(RestartGame, 30.0);
         AnchorPane.setRightAnchor(RestartGame, 20.0);
         
-        Button QuitGame = new Button("Quit?");
+        //loading a picture of a red x to add to the button
+        Image xImage;//try loading it from local classpath, and if not then from url
+        try{
+            xImage=new Image(getClass().getResource("Media/X.png").toExternalForm());
+        }
+        catch(Exception e){xImage=new Image("https://openclipart.org/image/800px/svg_to_png/15815/Arnoud999-Right-or-wrong-5.png");}
+        
+        ImageView xView=new ImageView(xImage);
+        xView.setFitHeight(20);
+        xView.setFitWidth(20);                
+        
+        Button QuitGame = new Button("Quit?",xView);
         QuitGame.setPrefSize(120, 50);
         QuitGame.setStyle("-fx-background-color: #3ADF00;");
         QuitGame.setTextFill(Color.BLUE);
+        
+        QuitGame.setOnMouseClicked(e ->{
+            closeGame=true;
+        });
         
         AnchorPane.setBottomAnchor(QuitGame, 30.0);
         AnchorPane.setLeftAnchor(QuitGame, 20.0);
@@ -60,3 +90,4 @@ public class LoseMenu extends Pane {
     }
 
 }
+
