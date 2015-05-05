9bbfb35f1939975eb95ef357bed93f3e42e8fbfc
AlexandraYepifanova <alexyepifanova269399@gmail.com>
2015-05-04 22:49:16 -0400

    Update MainGameplayPane.java

--- MainGameplayPane.java
+++ MainGameplayPane.java
@@ -1,3 +1,10 @@
+/*this is the pane for putting together the main gameplay
+it contains two canvas:
+the first will contain two lines forming an angle that you can make smaller and larger using 'w' and's' keys
+the second canvas contains an image of a ball and a target
+
+NOTE: if the postion of the ball isn't satisfactory, change START_BALL_X, and START_BALL_Y, and everything else moves to reflect that change
+*/
 package DunkAProf;
 
 import javafx.scene.layout.*;
@@ -20,26 +27,29 @@ import javafx.scene.paint.LinearGradient;
 
 
 public class MainGameplayPane extends Pane {
-    private final int WIDTH = 800;
-    private final int HEIGHT = 600;
+    private final int WIDTH = 800;//width of pane
+    private final int HEIGHT = 600;//height of pane
     
-    private final int BALL_X=60, BALL_Y=450;
-    private final int TARGET_X=705, TARGET_Y=350;
+    private final int START_BALL_X=60, START_BALL_Y=450;//where the ball will always be placed at the start
+    private int ballCurrentX=START_BALL_X, ballCurrentY=START_BALL_Y;//current x and y location of a ball
+    private final int TARGET_X=705, TARGET_Y=350;// x and y position of target
     
     boolean gameWon=false, gameLost=false, ballHit=false;
     
-    private Canvas ballCanvas=new Canvas(WIDTH,HEIGHT);
-    private GraphicsContext ballContext = ballCanvas.getGraphicsContext2D();
-    private Canvas angleCanvas= new Canvas(WIDTH,HEIGHT);
-    private GraphicsContext angleContext = angleCanvas.getGraphicsContext2D();
+    private Canvas ballTargetCanvas=new Canvas(WIDTH,HEIGHT);//the canvas will contain a ball and a target image
+    private GraphicsContext ballTargetContext = ballTargetCanvas.getGraphicsContext2D();
+    private Canvas anglePowerCanvas= new Canvas(WIDTH,HEIGHT);//the canvas will contain an angle and a power gauge
+    private GraphicsContext anglePowerContext = anglePowerCanvas.getGraphicsContext2D();
     
-    Image ball=new Image(getClass().getResource("Media/RedBall.png").toExternalForm());
-    Image target=new Image(getClass().getResource("Media/SidewaysTarget.png").toExternalForm());    
+    Image ball=new Image(getClass().getResource("Media/RedBall.png").toExternalForm());//image of a ball
+    Image target=new Image(getClass().getResource("Media/SidewaysTarget.png").toExternalForm());//image of a target    
     
-    private int startX=BALL_X+(int)(ball.getHeight()*0.04/2), startY=(int)(BALL_Y+(ball.getWidth()*0.04)/2);
-    private int endX=startX+125, endY=startY;
-    private int newEndX=endX, newEndY=endY;
-    private double angle=0;
+    /*this is for the creating of two lines that will form an angle*/
+    private final int ANGLE_PIVOT_X=START_BALL_X+(int)(ball.getHeight()*0.04/2), ANGLE_PIVOT_Y=(int)(START_BALL_Y+(ball.getWidth()*0.04)/2);/*point at which a line will rotate around*/
+    private final int END_OF_LINE_X=ANGLE_PIVOT_X+125, END_OF_LINE_Y=ANGLE_PIVOT_Y;
+    private int currentEndLineX=END_OF_LINE_X, currentEndLineY=END_OF_LINE_Y;//current x and y values of the end of a line that rotates around the pivot point
+    
+    private double angle=0;//will hold the current angle
     
     MainGameplayPane(){
         super.setHeight(HEIGHT);
@@ -47,9 +57,9 @@ public class MainGameplayPane extends Pane {
         
         this.setFocusTraversable(true);
         
-        draw();
+        draw();//draws the two lines forming an angle, the ball and the target
  
-        super.getChildren().addAll(ballCanvas, angleCanvas);
+        super.getChildren().addAll(ballTargetCanvas, anglePowerCanvas);
         
         this.setOnKeyTyped(e->{
             if(e.getCharacter().matches("w") || e.getCharacter().matches("W")){
@@ -64,38 +74,39 @@ public class MainGameplayPane extends Pane {
                     draw();
                 }
             } 
-            
         });
     }
     
     void draw(){
-        angleContext.clearRect(0, 0, WIDTH, HEIGHT);
-        ballContext.clearRect(0, 0, WIDTH, HEIGHT);
+        anglePowerContext.clearRect(0, 0, WIDTH, HEIGHT);
+        ballTargetContext.clearRect(0, 0, WIDTH, HEIGHT);
         
-        angleContext.setLineWidth(2.0);
-        angleContext.setStroke(Color.RED);
-        angleContext.strokeLine(startX,startY,endX, endY);
-        angleContext.strokeLine(startX,startY,newEndX, newEndY);
+        //drawing two lines that form an angle
+        anglePowerContext.setLineWidth(2.0);
+        anglePowerContext.setStroke(Color.RED);
+        anglePowerContext.strokeLine(ANGLE_PIVOT_X,ANGLE_PIVOT_Y,END_OF_LINE_X, END_OF_LINE_Y);
+        anglePowerContext.strokeLine(ANGLE_PIVOT_X,ANGLE_PIVOT_Y,currentEndLineX, currentEndLineY);
         
-        ballContext.drawImage(ball, BALL_X, BALL_Y, ball.getWidth()*0.04, ball.getHeight()*0.04);
-        ballContext.drawImage(target, TARGET_X, TARGET_Y, target.getWidth()*0.7, target.getHeight()*0.7);
+        //drawing a ball and a target
+        ballTargetContext.drawImage(ball, ballCurrentX, ballCurrentY, ball.getWidth()*0.04, ball.getHeight()*0.04);
+        ballTargetContext.drawImage(target, TARGET_X, TARGET_Y, target.getWidth()*0.7, target.getHeight()*0.7);
         
-        addGradiantRectangle();
+        addGradiantRectangle();//adds a gradient rectangle to be used for the power guage
     }
     
-    void rotateXY(double degrees){
+    void rotateXY(double degrees){//uses matrix multiplication to change x and y end-point values of line rotating around a pivot point
         angle=degrees;
-        newEndX=(int)(((endX-startX)*Math.cos(Math.toRadians(360-degrees)))-((endY-startY)*Math.sin(Math.toRadians(360-degrees)))+startX);
-        newEndY=(int)(((endX-startX)*Math.sin(Math.toRadians(360-degrees)))+((endY-startY)*Math.cos(Math.toRadians(360-degrees)))+startY);
+        currentEndLineX=(int)(((END_OF_LINE_X-ANGLE_PIVOT_X)*Math.cos(Math.toRadians(360-degrees)))-((END_OF_LINE_Y-ANGLE_PIVOT_Y)*Math.sin(Math.toRadians(360-degrees)))+ANGLE_PIVOT_X);
+        currentEndLineY=(int)(((END_OF_LINE_X-ANGLE_PIVOT_X)*Math.sin(Math.toRadians(360-degrees)))+((END_OF_LINE_Y-ANGLE_PIVOT_Y)*Math.cos(Math.toRadians(360-degrees)))+ANGLE_PIVOT_Y);
     }
     
-    void addGradiantRectangle(){
+    void addGradiantRectangle(){//draws a gradient rectangle for the power guage
         Stop[] stops=new Stop[]{new Stop(0, Color.LIGHTGREEN), new Stop(1, Color.RED)};
         
         LinearGradient lg=new LinearGradient(0,0,1,0, true, CycleMethod.NO_CYCLE, stops);
         
-        angleContext.setFill(lg);
-        angleContext.fillRect(BALL_X, BALL_Y+ball.getHeight()*0.04+10, 200, 20);
+        anglePowerContext.setFill(lg);
+        anglePowerContext.fillRect(START_BALL_X, START_BALL_Y+ball.getHeight()*0.04+10, 200, 20);
         
     }
 }
